2a7692118d16f6ddca6c1e5b3e5705d152b2c767
Toofi <herpoel.quentin@gmail.com>
2023-02-02 15:15:01 +0100

    removed app service & app controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ChallongeModule } from './challonge/challonge.module';
 
 @Module({
   imports: [ChallongeModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
